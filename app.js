7574b196fdbdd900235363298a803f3354fd9015
trunghieu <trunghieu0451@gmail.com>
2023-05-10 19:51:24 +0700

    add route and middleware

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-const http = require("http");
-
 const express = require("express");
 
 const app = express();
 
-const server = http.createServer(app);
+app.use("/add-product", (req, res, next) => {
+  res.send("<h1>Add Product Page</h1>");
+});
 
-server.listen(3000);
+app.use("/", (req, res, next) => {
+  res.send("<h1>Hello world!</h1>");
+});
+
+app.listen(3000);
